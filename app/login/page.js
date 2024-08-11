d27f262ab0f48f4f63c3a3c509c12f0d88fc4e88
jishnukm <jishnukm2010@gmail.com>
2024-08-11 15:26:33 +0530

    added demo login

--- app/login/page.js
+++ app/login/page.js
@@ -12,17 +12,71 @@ export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading,setIsLoading] =useState(false)
+  const [isLoadingDemo,setIsLoadingDemo] =useState(false)
   const router = useRouter();
+
+
+
+
+
+
+  async function handleDemoAccount(){
+
+    setIsLoadingDemo(true)
+
+    try {
+      const host_name = process.env.NEXT_PUBLIC_HOST_NAME;
+      const username = process.env.NEXT_PUBLIC_DEMO_USERNAME;
+      const password = process.env.NEXT_PUBLIC_DEMO_PASSWORD;
+
+
+
+      console.log(host_name,username,password)
+
+      
+       
+      const response = await fetch(`${host_name}/api-auth/login/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username:username,password:password }),
+        
+      });
+  
+      if(response.status == 401 || response.status == 404){
+
+        setIsLoading(false)
+        toast.error('Username or Password is Incorrect!');
+        throw new Error('Username or Password is Incorrect!');
+      }
+
+      if (!response.ok) {
+        setIsLoadingDemo(false)
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log(data)
+
+      localStorage.setItem('token', data.Token);
+
+      router.push('/home')
+    } catch (error) {
+      console.error('Error:', error);
+      setIsLoadingDemo(false)
+      
+    }
+
+
+
+  }
 
  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Add your login logic here
-    console.log('Login attempt with:', username, password);
-    // If login is successful, redirect to dashboard
-    // router.push('/dashboard');
-
+ 
     setIsLoading(true)
 
     try {
@@ -172,25 +226,42 @@ export default function Login() {
                 </span>
               </div>
             </div>
-
+            </div>
 
            
 
 
             <div className="mt-6">
               <button
-              disabled
-              title='Coming Soon'
-                onClick={handleGoogleLogin}
-                className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              
+                onClick={handleDemoAccount}
+                className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-white bg-[#10b981] hover:bg-[#059669] hover:cursor-pointer  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#059669]"
               >
-                <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                {/* <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z" fill="#4285F4"/>
-                </svg>
-                Sign in with Google
+                </svg> */}
+
+
+
+
+{isLoadingDemo == true ?
+                
+                <div role="status">
+    <svg aria-hidden="true" className="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-[#059669]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span className="sr-only">Loading...</span>
+</div>
+
+              
+  : 'Demo' }
+
+
+                
               </button>
             </div>
-          </div>
+          
         </div>
       </div>
     </div>
